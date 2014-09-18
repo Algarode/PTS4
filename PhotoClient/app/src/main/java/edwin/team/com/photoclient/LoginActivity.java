293c30587965d7a8ac4c566bd7bd2e90022370a1
I252443 <r.edelbroek@student.fontys.nl>
2014-09-18 11:15:32 +0200

    fixes in login layout and manifest

--- PhotoClient/app/src/main/java/edwin/team/com/photoclient/LoginActivity.java
+++ PhotoClient/app/src/main/java/edwin/team/com/photoclient/LoginActivity.java
@@ -2,6 +2,7 @@ package edwin.team.com.photoclient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class LoginActivity extends Activity {
@@ -10,5 +11,9 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+
     }
 }
