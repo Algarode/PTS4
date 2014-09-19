9a9fbf55959fc35b89a7d63cee2978eacb16c1d2
I252443 <r.edelbroek@student.fontys.nl>
2014-09-19 21:28:28 +0200

    fixes and updates
    registry intent gets loaded now, upload intent also gets loaded.
    keyboard doesnt appear automatically anymore. Possibility added to jump
    to next edittext. Loginscreen get closed now after going to the next
    intent. use finish to exit intents.

--- PhotoClient/app/src/main/java/edwin/team/com/photoclient/RegistrationActivity.java
+++ PhotoClient/app/src/main/java/edwin/team/com/photoclient/RegistrationActivity.java
@@ -1,7 +1,21 @@
 package edwin.team.com.photoclient;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class RegistrationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
 
+    public void register(View view) {
+        Intent intent = new Intent(this,uploadActivity.class);
+        startActivity(intent);
+    }
 }
--- PhotoClient/app/src/main/java/edwin/team/com/photoclient/uploadActivity.java
+++ PhotoClient/app/src/main/java/edwin/team/com/photoclient/uploadActivity.java
@@ -1,24 +1,14 @@
 package edwin.team.com.photoclient;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.WindowManager;
 
-
-public class LoginActivity extends Activity {
-
+public class uploadActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_upload);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
-    }
-
-    public void login(View view) {
-        Intent intent = new Intent(this,RegistrationActivity.class);
-        this.startActivity(intent);
-        finish();
     }
 }
